dceca3f687c996ef524559734cc61e745b92152a
Jefferson Lima <82480661+jeffinho-ns@users.noreply.github.com>
2023-11-08 08:46:01 -0300

    Update App.jsx

--- ToDo/src/App.jsx
+++ ToDo/src/App.jsx
@@ -3,7 +3,7 @@ import Todo from "./components/Todo";
 import './App.css'
 import TodoForm from './components/TodoForm';
 import Search from './components/Search';
-import Filter from './components/filter';
+import Filter from './components/Filter';
 
 function App() {
   const [todos, setTodos] = useState([
